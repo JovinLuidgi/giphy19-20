6d1a15857ed0e5b3b2f1b584bb9b47b25844cc50
JovinLuidgi <Jovinluidgi@gmail.com>
2020-02-11 23:10:50 +0000

    Change 1

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+$("button").click(function(){
+search = $("input").val;
+fetch("https://api.giphy.com/v1/gifs/search?q="+search+"&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        $(".results").append("<img src='"+ data.data[0].embed_url+"'>")
+    })
+});
